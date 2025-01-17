ff2962b8a43d5784bab8265d612414d5e135701f
Ig-ghost <deepakjoshig54@gmail.com>
2025-01-17 20:10:42 +0530

    Add files via upload

---
+++ revision.py
@@ -0,0 +1,102 @@
+#variables and data types
+
+name = 'Aaysha' #string
+number = '10' #string
+
+age = 25 #integer
+
+height = 5.9 #float
+
+is_student = True #boolean
+is_dev = False #boolean
+
+# this is comment
+
+# print('Hello World')
+# print(10)
+
+# print(name)
+# print(number)
+
+# Name : Aaysha
+#age : 25
+
+name = 'Sanjay'
+name = 'Rehnuma'
+name = 'Chandni'
+age = 20
+
+# print('Name :', name)
+# print('Age : ', age)
+# print('Height :', height)
+# print('is student :', is_student)
+
+# name is lucky and his age is 20 and his height is 5.9 and he is a bright student
+name = "sanjay"
+age = 18
+height = 6.0
+# print('name is lucky and his age is 20 and his height is 5.9 and he is a bright student')
+# print('name is', name, 'and his age is', age, 'and his height', height, 'and he is a bright student')
+
+name = 'Mahak'
+age = 20
+height = 5.8
+# print('name is', name, 'and her age is', age, 'and her height', height, 'and she is a bright student')
+
+#basic operators
+
+a = 2
+b = 4
+
+# # c = a + b
+# print(a + b)
+# print(a - b)
+# print(a / b) #float 
+# print(a // b) #floor division
+# print(a ** b) #exponentitation 2 * 2 * 2 * 2
+
+#strings
+
+welcome = 'hello WORLD'
+# print(welcome)
+#string operations
+# print(welcome.lower())
+# print(welcome.upper())
+# print(welcome.replace('WORLD', 'not'))
+
+name = 'Mohitt'
+#M -> 0, o -> 1, h -> 2.....
+# print(name[0])
+# print(name[1])
+# print(name[3])
+# print(name[5])
+
+name = 'mahak'
+# print(name[3])
+
+#conditional statements
+# age = 18
+# if age >= 18:
+#     print('You are eligible to drive')
+# elif 5 <= 5:
+#     print('sochna bhi mat')
+# else:
+#     print('you are not eligible')
+
+# print('done!!')
+
+#odd and even number checker
+
+# print(6 / 2)
+# print(6 % 4) #modulus operator -> remainder
+
+# print(5 == 5)
+# print(6 > 8)
+# print(6 < 8)
+# print(6 != 6)
+
+number = 9
+if number % 2 == 0:
+    print('number is even')
+else:
+    print('number is odd')
